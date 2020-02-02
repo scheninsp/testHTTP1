5d081049f08f65a29a14cd2c8416fdd1486529c4
scheninsp <songchen7788@163.com>
2020-02-03 00:29:54 +0800

    frontend animation
    frontend animation

--- app/src/main/java/com/example/testhttp1/MainActivity.java
+++ app/src/main/java/com/example/testhttp1/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.testhttp1;
 
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -20,10 +26,16 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     final String TAG = "MainActivity";
     static short serversLoadTimes = 0;
+
+    //paras for moving imageview
+    final int IMAGE_H = 200;
+    final int IMAGE_W = 160;
+    private int lastX, lastY;
+    private RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +44,18 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView text01 = (TextView)findViewById(R.id.text01);
 
+        //Animations
+        final ImageView iv1 = (ImageView)findViewById(R.id.iv);
+        final AnimationDrawable animDrawable1 = (AnimationDrawable) iv1.getBackground();
+
+        relativeLayout = (RelativeLayout) findViewById(R.id.layout1);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(IMAGE_W, IMAGE_H);
+        iv1.setLayoutParams(layoutParams);
+        iv1.setOnTouchListener(this);
+
+        animDrawable1.start();
+
+        //Buttons
         Button button01 = (Button) findViewById(R.id.button01);
         button01.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -116,5 +140,36 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    public boolean onTouch(View view, MotionEvent event){
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = (int) event.getRawX();
+                lastY = (int) event.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //计算出需要移动的距离
+                int dx = (int) event.getRawX() - lastX;
+                int dy = (int) event.getRawY() - lastY;
+                //将移动距离加上，现在本身距离边框的位置
+                int left = view.getLeft() + dx;
+                int top = view.getTop() + dy;
+                //获取到layoutParams然后改变属性，在设置回去
+                RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
+                        .getLayoutParams();
+                layoutParams.height = IMAGE_H;
+                layoutParams.width = IMAGE_W;
+                layoutParams.leftMargin = left;
+                layoutParams.topMargin = top;
+                view.setLayoutParams(layoutParams);
+                //记录最后一次移动的位置
+                lastX = (int) event.getRawX();
+                lastY = (int) event.getRawY();
+                break;
+        }
+        //刷新界面
+        relativeLayout.invalidate();
+        return true;
     }
 }
